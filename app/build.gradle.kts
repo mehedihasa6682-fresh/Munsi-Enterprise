669906645fac2c96ddfeae687deb992c400b7979
mehedihasa6682-fresh <mehedihasa6682@gmail.com>
2026-08-03 01:24:27 +0600

    build: automate keystore generation for CI builds
    Ensure a debug keystore is generated during the CI process if no signing keys are provided, preventing build failures due to missing keystore files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,13 +27,14 @@ android {
     create("release") {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       val kFile = file(keystorePath)
+      val debugKFile = file("${rootDir}/debug.keystore")
       if (kFile.exists()) {
         storeFile = kFile
         storePassword = System.getenv("STORE_PASSWORD") ?: "android"
         keyAlias = System.getenv("KEY_ALIAS") ?: "upload"
         keyPassword = System.getenv("KEY_PASSWORD") ?: "android"
       } else {
-        storeFile = file("${rootDir}/debug.keystore")
+        storeFile = debugKFile
         storePassword = "android"
         keyAlias = "androiddebugkey"
         keyPassword = "android"
